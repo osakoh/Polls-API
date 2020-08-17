8cb9d2799ba7bf93a5a5957d2e96b92c6a512fb1
Michael Osarodion <macmive@gmail.com>
2020-08-17 21:39:02 +0100

    updated path for ChoiceList in urls

--- polls/urls.py
+++ polls/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('polls/', PollList.as_view(), name='polls_list'),
     path('polls/<int:pk>/', PollDetail.as_view(), name='polls_detail'),
 
-    path('choices/', ChoiceList.as_view(), name='choice_list'),
+    path('polls/<int:pk>/choices/', ChoiceList.as_view(), name='choice_list'),
     path('vote/', CreateVote.as_view(), name='create_vote'),
 ]
